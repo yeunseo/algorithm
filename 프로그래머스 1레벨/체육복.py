790325145e5926b40d9df2698fea12201f9bab8f
Eunseo Yu <youesdia0704@gmail.com>
2021-01-16 11:51:07 +0900

    20210116

---
+++ 프로그래머스 1레벨/체육복.py
@@ -0,0 +1,20 @@
+n = 5
+lost = [2, 4]
+reserve = [1, 3, 5]
+
+
+def solution(n, lost, reserve):
+    answer = 0
+    reserve_set = set(reserve)-set(lost)
+    lost_set = set(lost)-set(reserve)
+
+    for i in reserve_set:
+        if i-1 in lost_set:
+            lost_set.remove(i-1)
+        elif i+1 in lost_set:
+            lost_set.remove(i+1)
+    answer = n-len(lost_set)
+    return answer
+
+
+solution(n, lost, reserve)
